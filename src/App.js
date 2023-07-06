54bdfc0ac02ae05bd1d1d878846ff96fb99cb50e
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-07-06 18:16:22 +0200

    steps, messages and buttons prepared for the click event

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 export default function App() {
+  const step = 1;
+
   return (
     <div className="steps">
       <div className="numbers">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
       </div>
-      <p className="message">We learn React</p>
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
       <div className="buttons">
         <button style={{ backgroundColor: "#7950f2", color: "white" }}>
           Previous
