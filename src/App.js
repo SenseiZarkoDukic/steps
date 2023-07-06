ddd7b6b56b7faa81785c967b46615dc9c8856ac9
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-07-06 17:27:11 +0200

    setting up App component with JSX

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
 export default function App() {
-  return <div>Hello React</div>;
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+      <div className="message">We learn React</div>
+      <button className="buttons">
+        <span>Left</span>
+        <span>Right</span>
+      </button>
+    </div>
+  );
 }
