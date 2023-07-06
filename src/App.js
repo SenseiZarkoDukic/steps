5e4187ca5c468fbdf6cd270bdd5fdb4cd82fdf13
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-07-06 17:41:16 +0200

    Setting inline styling of color to the buttons

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ export default function App() {
         <div>2</div>
         <div>3</div>
       </div>
-      <div className="message">We learn React</div>
+      <p className="message">We learn React</p>
       <div className="buttons">
-        <button>
-          <span>Left</span>
+        <button style={{ backgroundColor: "#7950f2", color: "white" }}>
+          Previous
         </button>
-        <button>
-          <span>Right</span>
+        <button style={{ backgroundColor: "#7950f2", color: "white" }}>
+          Next
         </button>
       </div>
     </div>
